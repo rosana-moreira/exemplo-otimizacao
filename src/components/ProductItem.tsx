aed4132a79e398b82f4682d994cf21679b0a3042
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-05-11 15:34:02 -0300

    Code Splitting

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,27 +1,42 @@
-import { memo } from "react";
+import { memo, useState } from 'react';
+import { AddProductToWishlistProps } from './AddProductToWishlist'
+import dynamic from 'next/dynamic';
+import lodash from 'lodash';
+
+const AddProductToWishlist = dynamic<AddProductToWishlistProps>(() => {
+  return import('./AddProductToWishlist').then(mod => mod.AddProductToWishlist)
+}, {
+  loading: () => <span>Carregando...</span>
+})
 
 interface ProductItemProps {
   product: {
     id: number;
-    prince: number;
+    price: number;
+    priceFormatted: string;
     title: string;
-  };
+  }
   onAddToWishlist: (id: number) => void;
 }
+
 function ProductItemComponent({ product, onAddToWishlist }: ProductItemProps) {
+  const [isAddingToWishlist, setIsAddingToWishlist] = useState(false);
+
   return (
     <div>
-      {product.title} - <strong>{product.prince}</strong>
-      <button onClick={() => onAddToWishlist(product.id)}>
-        Add to wishlist
-      </button>
+      {product.title} - <strong>{product.priceFormatted}</strong>
+      <button onClick={() => setIsAddingToWishlist(true)}>Adicionar aos favoritos</button>
+      
+      { isAddingToWishlist && (
+        <AddProductToWishlist
+          onAddToWishlist={() => onAddToWishlist(product.id)}
+          onRequestClose={() => setIsAddingToWishlist(false)}
+        />
+      ) }
     </div>
-  );
+  )
 }
 
-export const ProductItem = memo(
-  ProductItemComponent,
-  (prevProps, nextProps) => {
-    return Object.is(prevProps.product, nextProps.product);
-  }
-);
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+  return lodash.isEqual(prevProps.product, nextProps.product)
+})
--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,34 +1,63 @@
+import { List, ListRowRenderer } from 'react-virtualized'
+
 import { ProductItem } from "./ProductItem";
-import { useMemo } from "react";
+
 interface SearchResultsProps {
+  totalPrice: number;
   results: Array<{
     id: number;
-    prince: number;
+    price: number;
+    priceFormatted: string;
     title: string;
-  }>;
+  }>
   onAddToWishlist: (id: number) => void;
-}
-export default function SearchResults({
-  results,
-  onAddToWishlist,
-}: SearchResultsProps) {
-  const totalPrice = useMemo(() => {
-    return results.reduce((total, product) => {
-      return total + product.prince;
-    }, 0);
-  }, [results]);
+} 
+
+export function SearchResults({ totalPrice, results, onAddToWishlist }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem 
+          product={results[index]}
+          onAddToWishlist={onAddToWishlist}
+        />
+      </div>
+    );
+  }
+
   return (
     <div>
       <h2>{totalPrice}</h2>
-      {results.map((product) => {
-        return (
-          <ProductItem
-            key={product.id}
-            product={product}
-            onAddToWishlist={onAddToWishlist}
-          />
-        );
-      })}
+
+      <List 
+        height={300}
+        rowHeight={25}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   );
 }
+
+/**
+ * 1. Criar uma nova versão do componente
+ * 2. Comparar com a versão anterior
+ * 3. Se houverem alterações, vai atualizar o que alterou
+ */
+
+/**
+ * 1. Pure Functional Components
+ * 2. Renders too often
+ * 3. Re-renders with same props
+ * 4. Medium to big size
+ */
+
+/**
+ * useMemo / useCallback
+ * 
+ * 1. Cálculos pesados
+ * 2. Igualdade referencial (quando a gente repassa aquela informação a um componente filho)
+ * 
+ */
